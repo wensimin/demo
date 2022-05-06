9ca2700118954d337768f131c06a18d8b1267443
wensimin <hukaizhiyu2@gmail.com>
2022-05-07 07:57:19 +0800

    测试安全是否有效

--- websocket/src/main/kotlin/com/example/websocket/Config.kt
+++ websocket/src/main/kotlin/com/example/websocket/Config.kt
@@ -2,6 +2,8 @@ package com.example.websocket
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.web.reactive.HandlerMapping
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping
 import org.springframework.web.reactive.socket.WebSocketHandler
@@ -9,12 +11,17 @@ import org.springframework.web.reactive.socket.WebSocketHandler
 @Configuration
 class Config {
     @Bean
-    fun webSocketHandlerMapping(webSocketHandler: WebSocketHandler): HandlerMapping? {
-        val map: MutableMap<String, WebSocketHandler> = HashMap()
-        map["/event-emitter"] = webSocketHandler
-        val handlerMapping = SimpleUrlHandlerMapping()
-        handlerMapping.order = 1
-        handlerMapping.urlMap = map
-        return handlerMapping
+    fun webSocketHandlerMapping(webSocketHandler: WebSocketHandler): HandlerMapping {
+        return SimpleUrlHandlerMapping().apply {
+            order = 1
+            urlMap = mapOf("event-emitter" to webSocketHandler)
+        }
+    }
+
+    @Bean
+    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        return http.authorizeExchange {
+            it.pathMatchers("/event-emitter/**").permitAll()
+        }.build()
     }
 }
